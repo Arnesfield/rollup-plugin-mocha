88bd4b9827259b263e1fd5a306a13d6d715e3481
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-06-20 20:33:45 +0800

    Remove minify for rollup-test

--- rollup-test.config.js
+++ rollup-test.config.js
@@ -5,6 +5,6 @@ import pkg from './package.json';
 export default {
   input: 'test/index.spec.ts',
   output: { dir: 'tmp', format: 'cjs', entryFileNames: '[name].cjs' },
-  plugins: [mocha({ clear: true }), esbuild({ minify: true })],
+  plugins: [mocha({ clear: true }), esbuild()],
   external: Object.keys(pkg.devDependencies).concat('fs', 'path')
 };
